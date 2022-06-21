deeb33c56dc83d4cb83ffa90f5dc1d3f4664416a
hlowell <hlowell@student.21-school.ru>
2022-06-21 20:10:30 +0700

    Added more tests for BasicEventTest

--- src/test/java/ru/rumal/wishlist/integration/basic_event/BasicEventTest.java
+++ src/test/java/ru/rumal/wishlist/integration/basic_event/BasicEventTest.java
@@ -1,7 +1,7 @@
 package ru.rumal.wishlist.integration.basic_event;
 
-
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -23,8 +23,11 @@ import ru.rumal.wishlist.repository.BasicEventRepo;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -38,7 +41,6 @@ import static ru.rumal.wishlist.integration.utils.HttpResponseApiError.isApiErro
 @RequiredArgsConstructor
 @Import(TestUserFactory.class)
 @ExtendWith(MockitoExtension.class)
-@TestPropertySource(locations = "classpath:application-test.properties")
 public class BasicEventTest {
 
     @Autowired
@@ -50,6 +52,11 @@ public class BasicEventTest {
     @Autowired
     private MockMvc mockMvc;
 
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+
+    private final String[] nameList = {"Новый год", "МЖД", "День программиста"};
+    private final String[] dateList = {"2023-01-01 00:00", "2023-08-03 15:00", "2022-09-13 15:00"};
+
 
     @DisplayName("Return api error when user not authenticated")
     @Test
@@ -57,24 +64,16 @@ public class BasicEventTest {
         this.mockMvc
                 .perform(get("/api/v1/basic_event"))
                 .andDo(print())
-                .andExpect(status().isForbidden())
+                .andExpect(status()
+                        .isForbidden())
                 .andExpectAll(isApiError("Access denied", "FORBIDDEN"));
     }
 
-
-    @DisplayName("Return event when user authenticated")
-    @WithMockAppUser
-    @Test
-    public void createReturnEventWhenAuthenticated() throws Exception {
-        testUserFactory.initDB();
-        User user = testUserFactory.getCorrectUser();
-
-        String name = "Новый год";
-        String date = "2023-01-01 00:00";
-
+    @SneakyThrows
+    private void createEvent(String name, String date) {
         Map<String, String> eventMap = new HashMap<String, String>() {{
-            put("name", "Новый год");
-            put("date", "2023-01-01 00:00");
+            put("name", name);
+            put("date", date);
         }};
 
         this.mockMvc
@@ -84,18 +83,69 @@ public class BasicEventTest {
                         content().contentType("application/json"),
                         jsonPath("$.name", is(name)),
                         jsonPath("$.date", is(date)));
+    }
+
+    @DisplayName("Return event when user authenticated")
+    @WithMockAppUser
+    @Test
+    public void createReturnEventWhenAuthenticated() throws Exception {
+        testUserFactory.initDB();
+        User user = testUserFactory.getCorrectUser();
+
+        createEvent(nameList[0], dateList[0]);
 
         Optional<BasicEvent> optionalEvent = basicEventRepo.findById(1L);
 
         Assertions.assertTrue(optionalEvent.isPresent());
         BasicEvent event = optionalEvent.get();
-
-        LocalDateTime dateTime = LocalDateTime.parse(date, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
-
+        LocalDateTime dateTime = LocalDateTime.parse(dateList[0], formatter);
         Assertions.assertEquals(event.getId(), 1);
-        Assertions.assertEquals(event.getName(), name);
+        Assertions.assertEquals(event.getName(), nameList[0]);
         Assertions.assertEquals(event.getDate(), dateTime);
 
         testUserFactory.clearDB();
     }
+
+    @DisplayName("getAllWhenExists")
+    @WithMockAppUser
+    @Test
+    public void getAllWhenExists() throws Exception {
+        testUserFactory.initDB();
+        User user = testUserFactory.getCorrectUser();
+
+        for (int i = 0; i < 3; i++)
+            createEvent(nameList[i], dateList[i]);
+
+        List<BasicEvent> result = StreamSupport
+                .stream(basicEventRepo.findAll()
+                        .spliterator(), false)
+                .collect(Collectors.toList());
+
+        Assertions.assertEquals(result.size(), 3);
+
+        for (int i = 0; i < 3; i++) {
+            Assertions.assertEquals(result.get(i).getId(), i + 1);
+            Assertions.assertEquals(result.get(i).getName(), nameList[i]);
+            Assertions.assertEquals(result.get(i).getDate(), LocalDateTime.parse(dateList[i], formatter));
+        }
+        testUserFactory.clearDB();
+    }
+
+    @DisplayName("getAllWhenNotExist")
+    @WithMockAppUser
+    @Test
+    public void getAllWhenNotExist() throws Exception {
+        testUserFactory.initDB();
+        User user = testUserFactory.getCorrectUser();
+
+        List<BasicEvent> result = StreamSupport
+                .stream(basicEventRepo.findAll()
+                        .spliterator(), false)
+                .collect(Collectors.toList());
+        Assertions.assertEquals(result.size(), 0);
+
+        testUserFactory.clearDB();
+    }
+
+
 }
